ae52124ebbd19801b368b3a5a27ce1f7b883bb3c
Eli112358 <eli112358.sarp@hotmail.com>
2015-01-10 08:24:24 -0500

    Added get and set methods to stack Added .idea/codeStylesSettings.xml Minor code reformatting

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+string get(int index);
+string set(int index,string value);
+string get(int index) {
+	cout<<"Hello, World!"<<endl;
+	return 0;
+}
+string set(int index,string value) {
+	cout<<"Hello, World!"<<endl;
+	return 0;
 }
