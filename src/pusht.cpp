e5d8893b28f4beb8e7add6473a2f6c8e37e8887e
Eli112358 <eli112358.sarp@hotmail.com>
2015-01-11 03:32:08 -0500

    Added getFile,remove and stackSize methods to stack.cpp Renamed set to add Removed getName Removed first parameter of add Storing the stack in a file instead of env vars Filled in main in pusht.cpp Added help and replaceAll methods to pusht.cpp

--- src/pusht.cpp
+++ src/pusht.cpp
@@ -1,5 +1,37 @@
 using namespace std;
-int main() {
-	cout<<"Hello, World!"<<endl;
+int help();
+void replaceAll(string &str,const string &from,const string &to);
+int main(int argc,char *argv[]) {
+	if(argc>1&&(argv[1]=="/?"||argv[1]=="/help")) return help();
+	string newTitle;
+	for(int i=0;i<argc;i++) newTitle+=' '+argv[i];
+	newTitle.erase(0);
+	replaceAll(newTitle,"$prev",get(stackSize()-1));
+	if(newTitle=="") {
+		for(int i=size()-1;i>-1;--i) {
+			stack::show(i);
+		}
+	} else {
+		stack::add(newTitle);
+		system("title  "+newTitle);
+	}
 	return 0;
+}
+int help() {
+	printf("Usage:");
+	printf("  pusht [new title]");
+	printf("");
+	printf("Sets the window title to [new tite] and addes it to the stack");
+	printf("If no new title is specified (no args), then it displays the stack");
+	printf("");
+	return 0;
+}
+void replaceAll(string &str,const string &from,const string &to) {
+	if(from.empty())
+		return;
+	size_t start_pos=0;
+	while((start_pos=str.find(from,start_pos))!=std::string::npos) {
+		str.replace(start_pos,from.length(),to);
+		start_pos+=to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+	}
 }
--- src/stack.cpp
+++ src/stack.cpp
@@ -1,26 +1,68 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <bits/stringfwd.h>
 #include <bits/basic_string.h>
+#include <iosfwd>
+#include <bits/stl_algo.h>
+#include <fstream>
+#include <iostream>
 using namespace std;
+void add(string value);
 string get(int index);
-string getName(int index);
-void set(int index,char *value);
+char *getFile();
+string remove(int index);
 void show(int index);
+long stackSize();
+void add(string value) {
+	ofstream ofs(getFile(),ios::app);
+	if(!ofs.is_open()) {
+		cout<<"Unable to open file: "<<getFile()<<'\n';
+		return;
+	}
+	ofs.write(value.c_str(),value.npos);
+	ofs.flush();
+	ofs.close();
+}
 string get(int index) {
-	return getenv(getName(index).c_str());
+	ifstream ifs(getFile());
+	if(!ifs.is_open()) {
+		cout<<"Unable to open file: "<<getFile()<<'\n';
+		return "";
+	}
+	string line;
+	for(int i=0;getline(ifs,line);++i)
+		if(i==index) {
+			ifs.close();
+			return line;
+		}
+	return "";
 }
-string getName(int index) {
-	char name[]="title";
-	if(index>-1) name=name+index;
-	return name;
+char *getFile() {
+	return getenv("tStack");
 }
-void set(int index,string value) {
-	string name=getName(index);
-	string cmd="set ";
-	cmd+='"'+name+'='+value+'"';
-	system(cmd.c_str());
-};
+string remove(int index) {
+	string removed=get(index);
+	ifstream ifs(getFile());
+	if(!ifs.is_open()) {
+		cout<<"Unable to open file: "<<getFile()<<'\n';
+		return "";
+	}
+	vector<string> prev;
+	string line;
+	while(getline(ifs,line)) prev.push_back(line);
+	ifs.close();
+	ofstream ofs(getFile(),ios::trunc);
+	if(!ofs.is_open()) {
+		cout<<"Unable to open file: "<<getFile()<<'\n';
+		return "";
+	}
+	for(int i=0;i<prev.size();++i) if(i!=index) ofs<<prev(i)<<'\n';
+	ofs.flush();
+	ofs.close();
+	return removed;
+}
 void show(int index) {
-	printf(get(index).c_str());
+	cout<<get(index)<<'\n';
+}
+long stackSize() {
+	return count(istreambuf_iterator<char>(ifstream(getFile())),istreambuf_iterator<char>(),'\n');
 }
