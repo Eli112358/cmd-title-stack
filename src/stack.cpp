37a843ed62ca504ed2e3d02019fe842f9af2604b
Eli112358 <eli112358.sarp@hotmail.com>
2015-01-10 20:07:03 -0500

    Added getName and show methods to stack Method set:  - Changed return type to void  - Changed second parameter type to string Filled in all four method bodies

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,12 +1,26 @@
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <bits/stringfwd.h>
+#include <bits/basic_string.h>
 using namespace std;
 string get(int index);
-string set(int index,string value);
+string getName(int index);
+void set(int index,char *value);
+void show(int index);
 string get(int index) {
-	cout<<"Hello, World!"<<endl;
-	return 0;
+	return getenv(getName(index).c_str());
 }
-string set(int index,string value) {
-	cout<<"Hello, World!"<<endl;
-	return 0;
+string getName(int index) {
+	char name[]="title";
+	if(index>-1) name=name+index;
+	return name;
+}
+void set(int index,string value) {
+	string name=getName(index);
+	string cmd="set ";
+	cmd+='"'+name+'='+value+'"';
+	system(cmd.c_str());
+};
+void show(int index) {
+	printf(get(index).c_str());
 }
